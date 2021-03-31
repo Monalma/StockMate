81777005bb19ef755c7aff2bdfc5dc46abc14a5b
Sanjith07 <spabbise@purdue.edu>
2021-03-31 16:52:52 -0400

    Added Backend connection with GCP database

--- Code/BackEnd/test.py
+++ Code/BackEnd/test.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
 from flask import Flask, redirect, url_for, request
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+from flask import Flask, request, make_response
+from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
+
+import connection_info
 app = Flask(__name__)
+
+cnx = mysql.connector.connect(user=connection_info.MyUser, password=connection_info.MyPassword,
+                                  host=connection_info.MyHost,
+                                  database=connection_info.MyDatabase)
+mysql = MySQL(app)
+cursor = cnx.cursor()
 
 @app.route('/dashboard/<a>/<b>')
 def dashboard(a, b):
@@ -14,6 +26,14 @@ def signup():
    if request.method == 'POST':
       firstName = request.form['firstName']
       lastName = request.form['lastName']
+      email = request.form['email']
+      password = request.form['password']
+      query = "INSERT INTO user (emailID, firstName, lastName, password) VALUES ('" + email+"', '"+ firstName +"', '"+lastName +"', '" + password+"');"
+      print(query)
+      cursor.execute(query)
+      cnx.commit()
+      cursor.close()
+      cnx.close()
       return redirect(url_for('dashboard',a = firstName, b = lastName))
    if request.method == 'GET':
       firstName = request.args.get('firstName')
