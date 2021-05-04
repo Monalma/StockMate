1fa8de384d66d991354909c5aff8bf0084158de2
Sanjith07 <spabbise@purdue.edu>
2021-05-04 01:09:38 -0400

    Analytics Functionality Added
    Changed all raw SQL to Prepared Statements, Added Stored Procedures

--- Code/BackEnd/start.py
+++ Code/BackEnd/start.py
@@ -6,6 +6,7 @@ from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
 from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask,render_template,request,redirect,flash
 import mysql.connector
 
 import connection_info
@@ -17,15 +18,21 @@ cnx = mysql.connector.connect(user=connection_info.MyUser, password=connection_i
 mysql = MySQL(app)
 cursor = cnx.cursor()
 
-@app.route('/dashboard/<a>/<b>')
-def dashboard(a, b):
+@app.route('/dashboard/<a>/')
+def dashboard(a):
    if request.method == 'GET':
       emailID = a
-      query = "select stockID, stockAmount, stockValue from userTransaction WHERE userAccountID = \""+ a +"\";"
-      print(query)
-      cursor.execute(query)
+      query = "select stockID, stockAmount, stockValue from userTransaction WHERE userAccountID = %s;"
+      print(query, a)
+      cursor.execute(query, (a,))
       allInfo = cursor.fetchall()
-      table = "<html><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Stockmate</title><link rel=\"stylesheet\" href=\"/static/styles.css\"><title>Document</title></head><body><h1 class=\"stock_text\">Your Stocks</h1><table id=\"customers\">"
+      table = "<html><head><meta charset=\"UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"><title>Stockmate</title><link rel=\"stylesheet\" href=\"/static/styles.css\"><title>Document</title></head><body>"
+      print("Printing the length")
+      print(allInfo)
+      if allInfo == []:
+         table += "<h1 class=\"stock_text\">You have no stock information!</h1></body></html>"
+         return table
+      table += "<h1 class=\"stock_text\">Your Stocks</h1><table id=\"customers\">"
       table += " <tr> <th> Stock ID </th> <th> Stock Amount </th>  <th> Stock Value</th> "
       for i in allInfo:
          stock_id = i[0]
@@ -33,7 +40,19 @@ def dashboard(a, b):
          stockValue = i[2]
          table += " <tr> <td>" + str(stock_id) + " </td> <td>" +str(stockAmount)+"</td> <td>" +str(stockValue)+ "</tr>"
       table += "</table>"
-      table += "<br></br><h1 class=\"stock_text\">Your Lists</h1></body> </html>"
+      table += "<br></br><h1 class=\"stock_text\">Your Lists</h1>"
+      table += """<h1 class=\"stock_text\">Your Statistics</h1><br></br>Total Gain/Loss: """
+      
+      args = (a, (0, 'CHAR'), (0, 'CHAR'))
+      result_args = cursor.callproc('GetNetAnalytics', args)
+      table += str(result_args[1]) + "$<br></br>"
+      table += "Percentage Gain/Loss: " + str((float(result_args[1])/float(result_args[2])) * 100) + "%<br></br>"
+      args2 = (a, (0, 'CHAR'), (0, 'CHAR'), (0, 'CHAR'))
+      result_args2 = cursor.callproc('GetTheBestStock', args2)
+      table += "Most Profitable Stock: " + result_args2[1] + "<br></br>"
+      table += "Price you bought " + result_args2[1] + " at: " + result_args2[2] + "$<br></br>"
+      table += "Price of " + result_args2[1] + " right now: " + result_args2[3] + "$<br></br>"
+      table += "</body> </html>"
       return table
    # else
    #    return 'welcome %s %s' % (a,b)
@@ -45,12 +64,10 @@ def signup():
       lastName = request.form['lastName']
       email = request.form['email']
       password = request.form['password']
-      query = "INSERT INTO user (emailID, firstName, lastName, password) VALUES ('" + email+"', '"+ firstName +"', '"+lastName +"', '" + password+"');"
-      print(query)
-      cursor.execute(query)
+      query = "INSERT INTO user (emailID, firstName, lastName, password) VALUES (%s, %s, %s, %s);"
+      print(query, (email, firstName, lastName, password))
+      cursor.execute(query, (email, firstName, lastName, password,))
       cnx.commit()
-      # cursor.close()
-      # cnx.close()
       return redirect(url_for('login'))
    if request.method == 'GET':
       firstName = request.args.get('firstName')
@@ -62,17 +79,18 @@ def login():
    if request.method == 'POST':
       email = request.form['email']
       password = request.form['password']
-      query = "SELECT COUNT(*) FROM user U WHERE U.emailID ='" + email + "' AND U.password ='" + password + "';"
-      cursor.execute(query)
+      query = "SELECT COUNT(*) FROM user U WHERE U.emailID = %s AND U.password = %s;"
+      cursor.execute(query, (email, password) )
       result = cursor.fetchall()
       print((result[0])[0])
       if (result[0])[0] == 1:
-         return redirect(url_for('dashboard',a = email, b = password))
+         return redirect(url_for('dashboard',a = email))
       else:
          print("Wrong User Name or Password!\n")
          return render_template('login.html')
 
    if request.method == 'GET':
+
       email = request.args.get('email')
       password = request.args.get('password')
       return render_template('login.html')
